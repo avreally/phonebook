eef194a880435ea3d6945eac631321ecede7fb47
Valeria-Shadrina <waleria.shadrina@gmail.com>
2021-05-28 17:32:02 +0200

    Chores

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+const Header = ({ course }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{course.name}</h1>
+  )
 }
 
-export default App;
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = (props) => {
+  // console.log(props);
+  return (
+    <div>
+      {props.part}
+    </div>    
+  )
+}
+
+const Content = ({ course }) => {
+  // console.log(course);
+  const result = course.parts.map(part =>
+    <p key={part.id}>{part.name} {part.exercises}</p>)
+  console.log(result);
+
+  return (
+    <div>
+      <Part part={result} />
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
