8f276124102216f1b86cf7329114eefc78933f1c
Valeria-Shadrina <waleria.shadrina@gmail.com>
2021-05-31 11:52:03 +0200

    2.5. Declared the Course component as a separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,48 +1,9 @@
-import React from 'react';
+import React from 'react'
+import Course from './components/Course'
 
 const MainHeading = ({ mainHeading }) => {
   return (
     <h1>{mainHeading}</h1>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-      <div>
-      <Header courseName={course.name} />
-      <div>{course.parts.map(part =>
-        <p key={part.id}>{part.name} {part.exercises}</p>)}</div>
-      <Total courses={course.parts} />
-    </div>
-  )
-}
-
-const Total = ({ courses }) => {
-  let initialValue = 0
-  const sum = courses.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.exercises, initialValue
-    )
-  return(
-    <p><b>total of {sum} exercises</b></p>
-  ) 
-}
-
-const Header = ({ courseName }) => {
-  return (
-    <div>
-      <h2>{courseName}</h2>
-    </div>
-  )
-}
-
-const Course = ({ courses }) => {
-  const coursesContent = courses.map(course =>
-    <Content key={course.id} course={course} />
-  )
-  return (
-    <div>
-      {coursesContent}
-    </div>
   )
 }
 
