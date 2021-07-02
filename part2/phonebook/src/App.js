bb0bc61f2ebc699460e72ba3e9c18ecd4127c52e
Valeria-Shadrina <waleria.shadrina@gmail.com>
2021-07-02 12:19:31 +0200

    Phonebook, step 6. Added fetching of the initial state of the data from the server using the axios-library with an Effect hook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ newFilter, handleFilter }) => {
   return (
@@ -60,15 +61,21 @@ const Persons = ({ persons, newFilter }) => {
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes');
 
   const addName = (event) => {
     event.preventDefault()
