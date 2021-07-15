00e109752713f1589c9c0243d44866d017fb9ec9
Valeria-Shadrina <waleria.shadrina@gmail.com>
2021-07-15 15:20:05 +0200

    Phonebook, step 7. Implemented saving added numbers to the backend server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,137 +1,132 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ newFilter, handleFilter }) => {
   return (
     <div>
-      filter shown with <input
-        value={newFilter}
-        onChange={handleFilter}
-      />
+      filter shown with <input value={newFilter} onChange={handleFilter} />
     </div>
-  )
-}
+  );
+};
 
-const Form = ({ addName, newName, handleNameChange, newNumber, handleNumberChange}) => {
-  return(
-    <form onSubmit={addName} >
-        <div>
-          name: <input 
-            value={newName}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          number: <input
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+const Form = ({
+  addName,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange,
+}) => {
+  return (
+    <form onSubmit={addName}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
 const Person = ({ persons }) => {
   return (
     <div>
-      {persons.map(person =>
+      {persons.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
-        )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const Persons = ({ persons, newFilter }) => {
   const filterItems = (arr, query) => {
-    return arr.filter(el => el.name.toLowerCase().includes(query.toLowerCase()))
-  }
-  
-  const filteredPersons = filterItems(persons, newFilter)
+    return arr.filter((el) =>
+      el.name.toLowerCase().includes(query.toLowerCase())
+    );
+  };
 
-  if (newFilter !== '') {
-    return <Person persons={filteredPersons}/>
+  const filteredPersons = filterItems(persons, newFilter);
+
+  if (newFilter !== "") {
+    return <Person persons={filteredPersons} />;
   } else {
-    return <Person persons={persons} />
+    return <Person persons={persons} />;
   }
-}
+};
 
 const App = () => {
-  const [ persons, setPersons ] = useState([])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ newFilter, setNewFilter ] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled');
-        setPersons(response.data)
-      })
-  }, [])
-  console.log('render', persons.length, 'notes');
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "notes");
 
   const addName = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const findName = persons.find(person => person.name === newName)
+    const findName = persons.find((person) => person.name === newName);
 
     if (findName !== undefined) {
-      window.alert(`${findName.name} is already added to phonebook`)
-      setNewName('')
-      setNewNumber('')
+      window.alert(`${findName.name} is already added to phonebook`);
+      setNewName("");
+      setNewNumber("");
     } else {
-      const allPersons = {
+      const newPerson = {
         name: newName,
         number: newNumber,
-      }
-  
-      setPersons(persons.concat(allPersons))
-      setNewName('')
-      setNewNumber('')
+      };
+
+      axios
+        .post("http://localhost:3001/persons", newPerson)
+        .then((response) => {
+          setPersons(persons.concat(newPerson));
+          setNewName("");
+          setNewNumber("");
+        });
     }
-  }
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  } 
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-  
+    setNewNumber(event.target.value);
+  };
+
   const handleFilter = (event) => {
-    setNewFilter(event.target.value)
-  }
+    setNewFilter(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter 
-        newFilter={newFilter} 
-        handleFilter={handleFilter}
-      />
+      <Filter newFilter={newFilter} handleFilter={handleFilter} />
       <h2>Add a new</h2>
-      <Form 
-        addName={addName} 
-        newName={newName} 
+      <Form
+        addName={addName}
+        newName={newName}
         handleNameChange={handleNameChange}
         newNumber={newNumber}
         handleNumberChange={handleNumberChange}
-       />
+      />
       <h2>Numbers</h2>
-      <Persons 
-        persons={persons} 
-        newFilter={newFilter}
-        />
+      <Persons persons={persons} newFilter={newFilter} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
